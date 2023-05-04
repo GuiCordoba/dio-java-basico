02860aa5172f69724021bcb9166b0492c369c1a5
Guilherme <guimcordoba@hotmail.com>
2023-05-04 16:29:36 -0300

    anatomia de classe

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -2,7 +2,14 @@ public class MinhaClasse {
 
     public static void main (String[] args) {
 
-        System.out.print("Olá, Mundo!");
+        String primeiroNome = "Guilherme";
+        String segundoNome = "Lopes";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println("->" + nomeCompleto);
     }
-    
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }   
 }
